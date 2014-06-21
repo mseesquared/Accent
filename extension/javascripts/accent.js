b94cb0b8c787194adc0fa28a39d659373a7f12b9
Yushi Wang <mseesquared@gmail.com>
2014-06-21 16:21:09 -0700

    added language codes

--- extension/javascripts/accent.js
+++ extension/javascripts/accent.js
@@ -3,4 +3,31 @@
   var startLang = prompt("What language will you be speaking in?");
   var endLang = prompt("What language do you want your speech to be translated to?");
 
+  var supportedLangs = [
+      "english",
+      "spanish",
+      "french",
+      "german",
+      "chinese"
+      "dutch",
+      "catalan",
+      "czech",
+      "finnish",
+      "polish",
+      "russian"
+    ];
+    var langAbbrevs = {
+      "english": "en-US",
+      "spanish": "es-US",
+      "french": "fr-FR",
+      "german": "de-DE",
+      "chinese": "cmn-Hans-CN"
+      "dutch": "nl-NL",
+      "catalan": "ca-ES",
+      "czech": "cs-CZ",
+      "finnish": "fi-FI",
+      "polish": "pl-PL",
+      "russian": "ru-RU"
+    };
+
 })();
