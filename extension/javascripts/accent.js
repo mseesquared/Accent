ca5da200e1fde53e7edee5d3acd1966d614564b5
Ray Zhou <rzhou186@stanford.edu>
2014-06-21 16:24:45 -0700

    Added base annyang code

--- extension/javascripts/accent.js
+++ extension/javascripts/accent.js
@@ -1,6 +1,18 @@
 (function(){
 
+  if (annyang) return;
+
   var startLang = prompt("What language will you be speaking in?");
   var endLang = prompt("What language do you want your speech to be translated to?");
+
+  var translate = function(utterance) {
+    // Pass utterance to John's server-side speech translation API.
+    // Pass the response into Yushi's client-side speech synthesis code.
+  }
+
+  var commands = { "*utterance": translate };
+
+  annyang.init(commands);
+  annyang.start();
 
 })();
