2acf5092ed61aba50a8759e063cc7b2f9821e658
Ray Zhou <rzhou186@stanford.edu>
2014-06-21 16:15:54 -0700

    Moved README.md to proper location

--- extension/javascripts/accent.js
+++ extension/javascripts/accent.js
@@ -1,1 +1,6 @@
-alert("Hello world!");
+(function(){
+
+  var startLang = prompt("What language will you be speaking in?");
+  var endLang = prompt("What language do you want your speech to be translated to?");
+
+})();
