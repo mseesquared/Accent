56dd37732f3921e562480575d7ec4b534ea2cb8b
Yushi Wang <mseesquared@gmail.com>
2014-06-21 19:03:57 -0700

    added translation codes

--- extension/javascripts/accent.js
+++ extension/javascripts/accent.js
@@ -5,17 +5,16 @@
   }
 
   var langAbbrevs = {
-    "english": "en-US",
-    "spanish": "es-US",
-    "french": "fr-FR",
-    "german": "de-DE",
-    "chinese": "cmn-Hans-CN",
-    "dutch": "nl-NL",
-    "catalan": "ca-ES",
-    "czech": "cs-CZ",
-    "finnish": "fi-FI",
-    "polish": "pl-PL",
-    "russian": "ru-RU"
+    "english": { "translate" : "en", "synthesis" : "en-US" },
+    "spanish": { "translate" : "es", "synthesis" : "es-US" },
+    "french": { "translate" : "fr", "synthesis" : "fr-FR" },
+    "german": { "translate" : "de", "synthesis" : "de-DE" },
+    "chinese": { "translate" : "zh-CHS", "synthesis" : "cmn-Hans-CN" },
+    "dutch": { "translate" : "nl", "synthesis" : "nl-NL" },
+    "czech": { "translate" : "cs", "synthesis" : "cs-CZ" },
+    "finnish": { "translate" : "fi", "synthesis" : "fi-FI" },
+    "polish": { "translate" : "pl", "synthesis" : "pl-PL" },
+    "russian": { "translate" : "ru", "synthesis" : "ru-RU" }
   };
 
   var sanitizeLang = function(lang) {
