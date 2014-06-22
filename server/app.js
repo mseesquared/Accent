9d17c9951130f40953e2edcc8c496a519515a25f
John YS <johnys@stanford.edu>
2014-06-21 21:30:38 -0700

    move git ignore

--- server/app.js
+++ server/app.js
@@ -10,6 +10,12 @@ var translate = require('./routes/translate');
 
 var app = express();
 
+var server = require =('http').Server(express);
+var io = require('socket.io')(server);
+server.listen(80);
+
+var connectionResponse = require('./connections/index')(io);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,8 +27,12 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// http
 app.use('/', routes);
 app.use('/translate', translate);
+
+// connections
+io.on('connection', connectionResponse);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/connections/index.js
@@ -0,0 +1,19 @@
+
+function connectionResponse(io) {
+
+  // The function
+  return function (socket) {
+    socket.on('utterance', function(utterance) {
+      /*
+      var hangoutUrl, userId, text;
+      hangoutUrl  = utterance.hangoutUrl;
+      userId      = utterance.userId;
+      text        = utterance.text;
+      */
+      console.log(utterance);
+      io.broadcast(utterance);
+    });
+  }
+}
+
+module.exports = connectionResponse;
